17f3b9d906735af1ce5611a1a2b68c1c27c64fa4
Ben Caldwell <caldwellb@uchicago.edu>
2022-01-10 23:48:08 -0600

    Add the rainbow mode and two color mode

--- dither.hs
+++ dither.hs
@@ -3,23 +3,44 @@ module Main (main) where
 import qualified Graphics.Image            as I
 import qualified Graphics.Image.Interface  as I.I
 import qualified Graphics.Image.ColorSpace as I.CS
+
+import Data.List
 import Data.Array
 import Data.Ix
 import Data.Foldable
+import Data.Maybe
+
 import System.IO
-import Control.Monad
 import System.Console.GetOpt
 import System.Exit
 import System.Environment
-import Data.Maybe
+import System.Random
 
-clamp :: Double -> I.Pixel I.Y Double -> I.Pixel I.Y Double
-clamp border = I.I.mapPxC (const $ (\x -> if x > border then 1.0 else 0.0) )
+import Control.Monad
+import Control.Monad.Primitive
 
-dither :: Double -> String -> IO (I.Image I.VU I.CS.Y Double) 
-dither border file = do
-  -- Read file as a greyscale (Y) image
-  toDither <- I.readImageY I.VU file
+clamp :: (RealFrac e, I.I.ColorSpace cs e, Monad m) => e -> I.Pixel cs e -> m (I.Pixel cs e)
+clamp border pxl = return $ I.I.mapPxC (const $ (\x -> if x > border then 1 else 0) ) pxl
+
+rainbowClamp :: Double -> I.Pixel I.RGB Double -> IO (I.Pixel I.RGB Double)
+rainbowClamp border pxl = do
+  if I.CS.toPixelY pxl < I.CS.PixelY border then do
+    r <- randomRIO (0,0.5)
+    g <- randomRIO (0,0.5)
+    b <- randomRIO (0,0.5)
+    return (I.CS.PixelRGB r g b) 
+    else do
+    r <- randomRIO (0.5,1) 
+    g <- randomRIO (0.5,1)
+    b <- randomRIO (0.5,1)
+    return (I.CS.PixelRGB r g b)
+
+clampTo :: Double -> I.Pixel I.RGB Double -> I.Pixel I.RGB Double -> I.Pixel I.RGB Double -> IO (I.Pixel I.RGB Double)
+clampTo border dark light pxl = if I.CS.toPixelY pxl < I.CS.PixelY border then return dark else return light
+
+dither :: (RealFrac e, I.I.ColorSpace cs e, PrimMonad m) => 
+  e -> (I.Pixel cs e -> m (I.Pixel cs e)) -> I.Image I.VU cs e -> m (I.Image I.VU cs e) 
+dither bound strategy toDither = do
   -- Get the bounds so we can avoid out of bounds errors
   let (x, y)    = I.I.dims toDither
       imgBounds = ((0,0), (x - 1, y - 1))
@@ -30,23 +51,24 @@ dither border file = do
     -- Look at the current pixel
     oldPixel <- I.I.read algoImg (x,y)
     -- Clamp the current pixel and get the error
-    let newPixel = clamp 0.5 oldPixel
-        quantErr = I.I.getPxC (oldPixel - newPixel) I.CS.LumaY 
+    newPixel <- strategy oldPixel
+    algPixel <- clamp bound oldPixel
+    let quantErr = oldPixel - algPixel
     -- Write the clamped pixel
     I.I.write algoImg (x,y) newPixel
     -- Overflow error into downstream pixels 
     when (inRange imgBounds (x  ,y+1)) 
          (I.I.read  algoImg (x  ,y+1) >>= \pxl -> 
-          I.I.write algoImg (x  ,y+1) $ I.I.liftPx (\x -> x + (7.0/16.0 * quantErr)) pxl) 
+          I.I.write algoImg (x  ,y+1) $ (pxl + (7/16 * quantErr))) 
     when (inRange imgBounds (x+1,y-1)) 
          (I.I.read  algoImg (x+1,y-1) >>= \pxl -> 
-          I.I.write algoImg (x+1,y-1) $ I.I.liftPx (\x -> x + (3.0/16.0 * quantErr)) pxl) 
+          I.I.write algoImg (x+1,y-1) $ (pxl + (3/16 * quantErr)))
     when (inRange imgBounds (x+1,y  )) 
          (I.I.read  algoImg (x+1,y  ) >>= \pxl -> 
-          I.I.write algoImg (x+1,y  ) $ I.I.liftPx (\x -> x + (5.0/16.0 * quantErr)) pxl) 
+          I.I.write algoImg (x+1,y  ) $ (pxl + (5/16 * quantErr)))
     when (inRange imgBounds (x+1,y+1)) 
          (I.I.read  algoImg (x+1,y+1) >>= \pxl -> 
-          I.I.write algoImg (x+1,y+1) $ I.I.liftPx (\x -> x + (1.0/16.0 * quantErr)) pxl) 
+          I.I.write algoImg (x+1,y+1) $ (pxl + (1/16 * quantErr)))
     return ()
   -- No more modifications, freeze the image and return.
   I.I.freeze algoImg
@@ -55,21 +77,29 @@ data Flag = Input String
           | Output (Maybe String)
           | Cutoff (Maybe Double)
 
-data Options = Options { optInput  :: Maybe String
-                       , optOutput :: String
-                       , optCutoff :: Double}
+data Options = Options { optInput   :: Maybe String
+                       , optOutput  :: String
+                       , optCutoff  :: Double
+                       , optRainbow :: Bool
+                       , optTwoCol  :: Maybe (I.Pixel I.RGB Double, I.Pixel I.RGB Double)}
 
 defaultOptions :: Options
-defaultOptions = Options { optInput  = Nothing 
-                         , optOutput = "dither.png"
-                         , optCutoff = 0.5 }
+defaultOptions = Options { optInput   = Nothing 
+                         , optOutput  = "dither.png"
+                         , optCutoff  = 0.5 
+                         , optRainbow = False
+                         , optTwoCol  = Nothing }
+
+readTwoCol :: String -> (I.Pixel I.RGB Double, I.Pixel I.RGB Double)
+readTwoCol xs = 
+  let vals = map read . words $ xs in
+      (I.CS.PixelRGB (vals !! 0) (vals !! 1) (vals !! 2) 
+      ,I.CS.PixelRGB (vals !! 3) (vals !! 4) (vals !! 5))
 
 options :: [ OptDescr (Options -> Options) ]
 options =
   [ Option "i" ["input"]
-      (OptArg
-        (\arg opt -> opt { optInput = arg })
-        "FILE")
+      (OptArg (\arg opt -> opt { optInput = arg }) "FILE")
       "Input file"
 
   , Option "o" ["output"]
@@ -77,29 +107,46 @@ options =
         (\arg opt -> opt { optOutput = arg })
         "FILE")
       "Output file"
+
   , Option "c" ["cutoff"]
       (ReqArg
         (\arg opt -> opt { optCutoff = read arg})
         "FLOAT")
       "Indicates brightness cutoff"
+  
+  , Option "r" ["rainbow"]
+      (NoArg (\opts -> opts {optRainbow = True}))
+      "use random colors in place of white"
+  
+  , Option "2" ["twocolor"]
+      (OptArg 
+        (\arg opt -> opt { optTwoCol = readTwoCol <$> arg } ) "DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE")
+      "RGB for dark and light sections"
   ]
 
 compilerOpts :: [String] -> IO (Options, [String])
 compilerOpts argv =
   case getOpt Permute options argv of
     (o,n,[]) -> return (foldl (flip id) defaultOptions o, n)
-    (_,_,errs) -> putStrLn (concat errs ++ usageInfo header options) >> die "Arguments improperly formatted" 
+    (_,_,errs) -> putStrLn (concat errs ++ usageInfo header options) >> die "" 
   where header = "Usage: dither [OPTION..] file"
 
 main :: IO ()
 main = do
   argv     <- getArgs
+  print argv
   (o,args) <- compilerOpts argv
-  case optInput o of
-    Nothing    -> case args of
-                    []     -> putStr (usageInfo ("Usage: dither [OPTION..] file") options) >> return ()
-                    (x:xs) -> dither (optCutoff o) x >>= I.writeImage (optOutput o)
-    Just strIn -> do
-      image    <- dither (optCutoff o) (strIn)
-      I.writeImage (optOutput o) image
+  mapM_ putStrLn args
+  let cutoff     = optCutoff o
+      ditherFunc = if (optRainbow o) 
+                    then dither cutoff (rainbowClamp cutoff) 
+                    else case (optTwoCol o) of
+                           Just (dark, light) -> dither cutoff (clampTo cutoff dark light)
+                           Nothing            -> dither cutoff (clamp cutoff)
+      targets    = case optInput o of
+                     Nothing -> args
+                     Just strIn -> strIn:args
+  for_ targets $ \tgt -> do
+    I.readImage' tgt >>= ditherFunc >>= I.writeImage ("dither" ++ (takeWhile (/= '.') tgt) ++ ".png")
+
   
